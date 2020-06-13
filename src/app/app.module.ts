b4d678b10984689b976fd7ecd35032dc7b30937f
David Martinez <deadkiller9906@gmail.com>
2020-06-12 19:10:32 -0500

    proyecto listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+import { ContenidoComponent } from './paginas/contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+   ContenidoComponent
   ],
   imports: [
     BrowserModule,
